9196a25b6e1cc17da2ad1f6828ead7901d950713
shakil <shakil.hossain450s@gmail.com>
2025-09-27 22:56:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+// middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+const pLanguages = [];
+
+app.get("/", (req, res) => {
+    res.status(200).render("index", {pLanguages});
+});
+
+app.post("/", (req, res) => {
+    const pLanguage = req.body.pLanguage;
+    pLanguages.push(pLanguage);
+    console.log(pLanguages);
+    res.redirect("/");
+    console.log(pLanguage);
+})
+
+module.exports = app;
